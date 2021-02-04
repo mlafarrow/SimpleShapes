00eba8ed7071e1d315b7964c03e38fd3a7019c56
emily farrow <ejfarrow@dons.usfca.edu>
2021-02-03 22:56:39 -0800

    Added Simple Shape files

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+  textSize(40);
+  textAlign(CENTER);
+
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(55);
+}
